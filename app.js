66ba1d6fa99c86df86bd2dedc776a537d5fd97fb
Yumik-xy <44742784+Yumik-xy@users.noreply.github.com>
2021-10-07 14:33:03 +0800

    增加了loading组件

--- app.js
+++ app.js
@@ -1,19 +1,16 @@
 // app.js
+
+import { promisifyAll } from 'wx-promise-pro'
+// https://github.com/youngjuning/wx-promise-pro
+import { updataInit } from 'wx-updata'
+// https://github.com/SHERlocked93/wx-updata
+
+promisifyAll()
+updataInit()
+
 App({
   onLaunch() {
-    // 展示本地存储能力
-    const logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
+    wx.pro.getSystemInfo().then(console.log)
+    Page = updataInit(Page, { debug: true })
   },
-  globalData: {
-    userInfo: null
-  }
 })
