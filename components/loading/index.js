9bdc1fe6905cbb758645adb7cd1247ffa1f07167
宇海 <44742784+Yumik-xy@users.noreply.github.com>
2021-10-10 09:43:41 +0800

    基本完成Loading和Card的组件制作 (#4)
    * 完成了userselectcard组件
    
    * 完成Select Card
    
    close #3 绘制完成，主页展示了前三种Select Identity Card
    
    * 繁琐的实现了双向绑定
    
    1. 在组件中使用wx-updata进行数据更新
    2. 通过`this.triggerEvent`实现双向数据传递，使得界面关闭的数据变化可以传递到Page中

--- components/loading/index.js
+++ components/loading/index.js
@@ -1,66 +1,58 @@
-// components/loading/index.js
-Page({
+import {
+    objToPath
+} from 'wx-updata'
+
+Component({
+    /**
+     * 组件的属性列表
+     */
+    properties: {
+        showModel: {
+            type: Boolean,
+            value: true
+        },
+        enableClose: {
+            type: Boolean,
+            value: true
+        },
+        title: {
+            type: String,
+            value: "数据请求中"
+        }
+    },
 
     /**
-     * 页面的初始数据
+     * 组件的初始数据
      */
     data: {
 
     },
 
     /**
-     * 生命周期函数--监听页面加载
+     * 组件的方法列表
      */
-    onLoad: function (options) {
+    methods: {
+        upData(data) {
+            return this.setData(objToPath(data))
+        },
 
+        // 阻止向下的滚动传递
+        touchMove() {
+
+        },
+
+        tap() {
+            if (this.properties.enableClose == true) {
+                this.closeModel()
+            }
+        },
+
+        // 关闭
+        closeModel() {
+            this.upData({
+                showModel: false
+            })
+            this.triggerEvent('show-model-changed', false)
+        }
     },
-
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
-
-    }
 })
--- components/userselectcard/index.js
+++ components/userselectcard/index.js
@@ -1,66 +1,37 @@
-// components/userselectcard/index.js
-Page({
+Component({
+    /**
+     * 组件的属性列表
+     */
+    properties: {
+        color: {
+            type: String,
+            value: "blue"
+        },
+        title: {
+            type: String,
+            value: "我是实验室管理"
+        },
+        subtitle: {
+            type: String,
+            value: "Administrator"
+        },
+        image: {
+            type: String,
+            value: "/images/icon-administrator.svg"
+        }
+    },
 
     /**
-     * 页面的初始数据
+     * 组件的初始数据
      */
     data: {
-
+        
     },
 
     /**
-     * 生命周期函数--监听页面加载
+     * 组件的方法列表
      */
-    onLoad: function (options) {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
+    methods: {
 
     }
 })
