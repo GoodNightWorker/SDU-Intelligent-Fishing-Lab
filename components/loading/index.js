f75174728990e2f48d44f3aeb10a1f558c3e9a69
Yumik-xy <44742784+Yumik-xy@users.noreply.github.com>
2021-10-10 09:00:14 +0800

    完成Select Card
    close #3 绘制完成，主页展示了前三种Select Identity Card

--- components/loading/index.js
+++ components/loading/index.js
@@ -3,20 +3,55 @@ Component({
      * 组件的属性列表
      */
     properties: {
-
+        showModel: {
+            type: Boolean,
+            value: true
+        },
+        enableClose: {
+            type: Boolean,
+            value: true
+        },
+        title: {
+            type: String,
+            value: "数据请求中"
+        }
     },
 
     /**
      * 组件的初始数据
      */
     data: {
-
+        dShowModel: true
     },
 
     /**
      * 组件的方法列表
      */
     methods: {
+        // 阻止向下的滚动传递
+        touchMove() {
 
-    }
+        },
+
+        tap() {
+            this.closeModel()
+        },
+
+        // 关闭
+        closeModel() {
+            if (this.properties.enableClose == true) {
+                this.setData({
+                    dShowModel: false
+                })
+            }
+        }
+    },
+
+    observers: {
+        'showModel': function (showModel) {
+            this.setData({
+                dShowModel: showModel
+            })
+        }
+    },
 })
