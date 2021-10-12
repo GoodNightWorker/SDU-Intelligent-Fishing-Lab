eb8c1db6ee1d6c8757e0c298fed57fc5ee337f9e
Like_Frost <49356858+LikeFrost@users.noreply.github.com>
2021-10-12 20:22:08 +0800

    add noticeitem (#12)
    * 实现 Notice Item
    Fixes #6
    
    * 调整部分布局样式
    
    * 调整部分布局样式
    
    Co-authored-by: Yumik-xy <44742784+Yumik-xy@users.noreply.github.com>

---
+++ components/noticeitem/index.js
@@ -0,0 +1,55 @@
+import { objToPath } from "wx-updata";
+
+Component({
+    /**
+     * 组件的属性列表
+     */
+    properties: {
+        icon: {
+            type: String,
+            value: "/images/userleft.svg",
+        },
+        message : {
+            type : String,
+            value : "人员离开",
+        },
+        time : {
+            type : String,
+            value : "14:30",
+        },
+        color: {
+            type : String,
+            value : "red",
+        },
+        detail : {
+            type : String,
+            value : "22°C",
+        }
+    },
+
+    /**
+     * 组件的初始数据
+     */
+    data: {},
+
+    /**
+     * 组件的方法列表
+     */
+    methods: {
+        upData(data) {
+            return this.setData(objToPath(data));
+        },
+
+        // 阻止向下的滚动传递
+        touchMove() {},
+
+        tap() {
+            this.closeModel();
+        },
+
+        // 关闭
+        closeModel() {
+            this.triggerEvent("closeCallBack");
+        },
+    },
+});
