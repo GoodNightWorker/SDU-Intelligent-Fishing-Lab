893b77c3ab9c082026bb9a885ae6eae5ed65ab9a
Like Frost <1342230493@qq.com>
2021-12-06 11:04:36 +0800

    canvas像素比

--- pages/index/labinfo/labkeylist/labkeyinfo/sharekey/index.js
+++ pages/index/labinfo/labkeylist/labkeyinfo/sharekey/index.js
@@ -61,66 +61,47 @@ Page({
         })
     },
     drawPic(){
-        console.log('drawPic')
-        const query = wx.createSelectorQuery()
-        query.select('#canvas')
-        .fields({node:true,size:true})
-        .exec((res)=>{
-            console.log('获取节点',res)
-            const canvas = res[0].node;
-            const dpr = wx.getSystemInfoSync().pixelRatio
-            canvas.width = res[0].width * dpr
-            canvas.height = res[0].height * dpr
-            const ctx = canvas.getContext('2d');
-            let img = canvas.createImage();
-            img.src = "/images/Subtract.svg";
-            img.onload = () =>{
-                console.log('图片加载完成')
-                ctx.drawImage(img,1,1,canvas.width,canvas.height);
-                ctx.textBaseline = "top";
-                ctx.textAlign = "center"
-                ctx.font = `600 ${24*dpr}px 黑体`;
-                ctx.fillStyle="#333333";
-                ctx.fillText(this.data.labName,res[0].width * dpr/2,24*dpr);
-                ctx.font = `400 ${14*dpr}px 黑体`;
-                ctx.fillStyle="#666666";
-                ctx.textAlign = "left"
-                ctx.fillText(`管理员：${this.data.userName}`,74*dpr,78*dpr);
-                ctx.fillText(`联系电话：${this.data.telephone}`,60*dpr,106*dpr);
-                ctx.fillText(`密钥时限：${this.data.duration}`,60*dpr,134*dpr);
-                ctx.fillText(`成员时限：${this.data.deadline}`,60*dpr,162*dpr);
-                ctx.textAlign = "center"
-                ctx.fillStyle="#C74242";
-                ctx.fillText("请注意实验室相关事项，注意实验安全",res[0].width * dpr/2,210*dpr);
-                ctx.fillText('实验室',307*dpr,319*dpr)
-                const buffer = wx.base64ToArrayBuffer(this.data.keyImg),
-                filePath = `${wx.env.USER_DATA_PATH}/temp_image.png`;
-                wx.getFileSystemManager().writeFile({ 
-                    filePath,
-                    data: buffer,
-                    encoding: 'base64',
-                    success:()=>{
-                        console.log('写成功')
-                        const code = canvas.createImage();
-                        code.src = filePath;
-                        code.onload=()=>{
-                            console.log('code加载成功')
-                            ctx.drawImage(code,25*dpr,268*dpr,88*dpr,88*dpr)
-                        }
-                    }
-                })
-                ctx.textAlign = "left";
-                ctx.fillStyle="#585858";
-                ctx.fillText(`${this.data.shareName}已经加入实验室`,130*dpr,291*dpr)
-                ctx.fillText('长按小程序码，立即加入',130*dpr,319*dpr)
+        const ctx = wx.createCanvasContext('canvas');
+        const dpr = wx.getSystemInfoSync().pixelRatio
+        ctx.scale(2,2);
+        ctx.drawImage("/images/Subtract.svg",0,0,176,183);
+        ctx.textBaseline = "top";
+        ctx.textAlign = "center"
+        ctx.font = `600 ${12}px 黑体`;
+        ctx.fillStyle="#333333";
+        ctx.fillText(this.data.labName,176/2,12);
+        ctx.font = `400 ${14}px 黑体`;
+        ctx.fillStyle="#666666";
+        ctx.textAlign = "left"
+        ctx.fillText(`管理员：${this.data.userName}`,74,78);
+        ctx.fillText(`联系电话：${this.data.telephone}`,60,106);
+        ctx.fillText(`密钥时限：${this.data.duration}`,60,134);
+        ctx.fillText(`成员时限：${this.data.deadline}`,60,162);
+        ctx.textAlign = "center"
+        ctx.fillStyle="#C74242";
+        ctx.fillText("请注意实验室相关事项，注意实验安全",352/2,210);
+        ctx.fillText('实验室',307,319);
+        ctx.textAlign = "left";
+        ctx.fillStyle="#585858";
+        ctx.fillText(`${this.data.shareName}已经加入实验室`,130,291)
+        ctx.fillText('长按小程序码，立即加入',130,319);
+        const buffer = wx.base64ToArrayBuffer(this.data.keyImg),
+        filePath = `${wx.env.USER_DATA_PATH}/temp_image.png`;
+        wx.getFileSystemManager().writeFile({ 
+            filePath,
+            data: buffer,
+            encoding: 'base64',
+            success:()=>{
+                console.log('写成功')
+                ctx.drawImage(filePath,25,268,88,88);
+                ctx.draw();
             }
-            ctx.restore();
         })
     },
     shareImg(){
         const query = wx.createSelectorQuery()
         const dpr = wx.getSystemInfoSync().pixelRatio
-        query.select('#canvas')
+        query.select('.canvas')
         .fields({node:true,size:true})
         .exec((res)=>{
             const canvas = res[0].node;
